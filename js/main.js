c2dbfb2d51fcb88006ea1413f6f24ce79869049b
Andrey Petrenko <123petrotreiler321@gmail.com>
2020-04-27 16:20:13 +0300

    add video and nav menu

--- js/main.js
+++ js/main.js
@@ -5,4 +5,12 @@ $(function () {
         autoplay: 2000,
         dots: true
     });
+
+
+    $('.header-btn').on('click', function () {
+        $('.menu').addClass('active');
+    })
+    $('.close-btn').on('click', function () {
+        $('.menu').removeClass('active')
+    })
 });
